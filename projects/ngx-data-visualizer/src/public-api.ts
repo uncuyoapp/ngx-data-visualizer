e7647cdcb26b3ce31653003cb939abd28f0306b2
gonzasiman <gonzasiman@users.noreply.github.com>
2026-06-24 12:05:16 -0300

    feat(lib/providers): exportar nuevos servicios y tipos en la api pública

--- projects/ngx-data-visualizer/src/public-api.ts
+++ projects/ngx-data-visualizer/src/public-api.ts
@@ -2,10 +2,13 @@
  * Public API Surface of ngx-data-visualizer
  */
 
-export * from "./lib/services/dataset";
-export * from "./lib/table/services/theme.service";
 export * from "./lib/chart/chart.component";
 export * from "./lib/multiple-chart/multiple-chart.component";
+export * from "./lib/providers";
+export * from "./lib/services/audit.service";
+export * from "./lib/services/dataset";
+export * from "./lib/services/event-bus.service";
+export * from "./lib/table/services/theme.service";
 export * from "./lib/table/table.component";
 export * from "./lib/types/data.types";
-export * from "./lib/providers";
+export * from "./lib/types/visualizer-event.types";

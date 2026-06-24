b124251e098cd232441ab97d69eb9c401f5c38e7
gonzasiman <gonzasiman@users.noreply.github.com>
2026-06-24 14:44:07 -0300

    feat(demo/app): añadir ruta y menú para laboratorio de tamaños

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { FullExampleComponent } from "./full-example/full-example.component";
 import { HomeComponent } from "./home/home.component";
 import { MultichartDemoComponent } from "./multichart-demo/multichart-demo.component";
 import { TableDemoComponent } from "./table-demo/table-demo.component";
+import { SizeTestComponent } from "./size-test/size-test.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -17,4 +18,5 @@ export const routes: Routes = [
   { path: "configuration", component: ConfigurationComponent },
   { path: "multichart-demo", component: MultichartDemoComponent },
   { path: "chart-cases-test", component: ChartCasesTestComponent },
+  { path: "size-test", component: SizeTestComponent },
 ];
